3f951d306aa39d61c5b15513205c8d9d803bea46
M.Sumanth <86638933+Suman-Sai@users.noreply.github.com>
2025-02-11 16:16:35 +0530

    Jokesgenerator.py

---
+++ jokesgenerator.py
@@ -0,0 +1,19 @@
+import pyjokes
+
+# Fetch the joke
+joke1 = pyjokes.get_joke(language="en", category="all")
+
+# Display the joke
+print(joke1,"\n")
+
+# Different category
+joke2 = pyjokes.get_joke(language="en", category="neutral")
+
+# Display the joke
+print(joke2,"\n")
+
+# Fetch multiple jokes
+jokes = pyjokes.get_jokes(language="en", category="neutral")
+
+for i in range(5):
+    print(i+1,".",jokes[i],"\n")
